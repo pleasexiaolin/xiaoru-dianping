ee2d89c1fb2c1c87f9e99ff2b05194b037d11197
xiaolin <pleasexiaolin@gmail.com>
2025-11-23 23:27:58 +0800

    info：项目初始化 user：小林 data：2025/11/23

---
+++ src/test/java/com/xiaolin/XiaoruDianpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaolin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoruDianpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
